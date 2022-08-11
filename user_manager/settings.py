f2640415a6d5949943fa43f0671deaeb0a6d4313
fernando158 <fernando.salas2102@gmail.com>
2022-08-11 01:04:09 -0400

    Archivos necesarios para Heroku

--- user_manager/settings.py
+++ user_manager/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ')p%*%m6eirj0l%*isxp@-_hob=9&=yf7*!*d55rtnsm-d^yv6#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
--- user_manager/urls.py
+++ user_manager/urls.py
@@ -13,10 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth.models import User
-from rest_framework import serializers, viewsets
 from rest_framework.routers import DefaultRouter
 #from rest_framework.authtoken import views
 from user_manager import views
